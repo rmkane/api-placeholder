e8c6fb71c8dbe4d57b0e34a00ad8ecf2d9052e91
ryankane <rmkane89@gmail.com>
2022-11-07 20:27:05 -0500

    Add remaining REST verbs for posts

--- app.js
+++ app.js
@@ -94,6 +94,75 @@ app.get(`/api/posts/:id`, (req, res) => {
 app.get('/api/posts/:id/comments', (req, res) => {
     res.send(database.comments.filter(({ postId }) => postId === parseInt(req.params['id'], 10)));
 });
+app.post('/api/posts', (req, res) => {
+    const {
+        title = '',
+        body = '',
+        userId = database.users[0].id
+    } = req.body ?? {};
+    const newPost = {
+        id: database.posts.length + 1,
+        title,
+        body,
+        userId,
+    };
+    res.send(newPost);
+});
+app.put(`/api/posts/:id`, (req, res) => {
+    const {
+        id,
+        title,
+        body,
+        userId,
+    } = req.body ?? {};
+    if (id == null || id !== parseInt(req.params['id'], 10)) {
+        res.status(400).send({
+            message: `The provided id '${id}' does not match '${req.params['id']}'`
+        });
+        return;
+    }
+    const replacedPost = {
+        id,
+        title,
+        body,
+        userId,
+    };
+    res.send(replacedPost);
+});
+app.patch(`/api/posts/:id`, (req, res) => {
+    const existingPost = database.posts.find(({ id }) => id === parseInt(req.params['id'], 10));
+    const {
+        id = existingPost.id,
+        title = existingPost.title,
+        body = existingPost.body,
+        userId = existingPost.userId,
+    } = req.body ?? {};
+    if (id == null || id !== parseInt(req.params['id'], 10)) {
+        res.status(400).send({
+            message: `The provided id '${id}' does not match '${req.params['id']}'`
+        });
+        return;
+    }
+    const updatedPost = {
+        id,
+        title,
+        body,
+        userId,
+    };
+    res.send(updatedPost);
+});
+app.delete('/api/posts/:id', (req, res) => {
+    const existingPost = database.posts.find(({ id }) => id === parseInt(req.params['id'], 10));
+    if (existingPost) {
+        res.status(200).send({
+            message: 'Post successfully deleted'
+        });
+    } else {
+        res.status(404).send({
+            message: `Post with id ${req.params['id']} does not exist`
+        });
+    }
+});
 
 // Todos
 app.get(`/api/todos`, (req, res) => {
